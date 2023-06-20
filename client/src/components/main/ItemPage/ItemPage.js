eb87d314786267b2aef7ed76fc441f768de8c4e4
SeanHaugen <sean.haugen560@gmail.com>
2023-06-20 15:11:57 -0500

    Related Items component draft

--- client/src/components/main/ItemPage/ItemPage.js
+++ client/src/components/main/ItemPage/ItemPage.js
@@ -5,10 +5,11 @@ import SelfPromo from "./selfPromo/SelfPromo";
 import ColorBox from "./colorGrid/ColorBox";
 import './itemPg.css';
 import RelatedItems from "./relatedItems/RelatedItems";
+import FedexTool from "./fedexTool/FedexTool";
 
-function ItemPage({setPin, item}) {
+function ItemPage({setPin, item, searchData}) {
     
-    console.log(item);
+    // console.log(item);
 
     const [products, setProducts] = useState([]);
     const [error, setError] = useState('');
@@ -32,7 +33,7 @@ function ItemPage({setPin, item}) {
       return <div>{error}</div>;
     }
 
-    console.log(products);
+    // console.log(products);
 
     const handlePin = () => {
       setPin((prevPin) => [...prevPin, item]);
@@ -44,26 +45,29 @@ function ItemPage({setPin, item}) {
       <div className='box content'>        
         <div className='product-list'>
           {products.map((product) => (
-            <>
-            {/* <h2>{product.category}</h2> */}
-            <div key={product.item_number} className='product-list-items'>
-              <div>
-               <figure>
-                  <img className="placeholder" src="https://fakeimg.pl/400x300"></img>
-               </figure>
-               </div>
-               <h1>{product.name}</h1>
-               <h2>{product.item_number}</h2> 
-               <p>{product.description}</p> 
-               <ItemPricingTable item={item} />
-               
-              
+            <div key={product.item_number} >
+              <h2>{product.category}</h2>
+              <div className='product-list-items'>
+                <div>
+                <figure>
+                    <img className="placeholder" src="https://fakeimg.pl/400x300" alt="placeholder blank"></img>
+                </figure>
+                </div>
+                <h1>{product.name}</h1>
+                <h2>{product.item_number}</h2> 
+                <p>{product.description}</p> 
+                <p>{product.kit_includes}</p>
+                <ItemPricingTable item={item} />
+                
+                
+              </div>
+              <button onClick={handlePin}>Save to Favs</button>
+              <SelfPromo product={product} />
+              <ColorBox product={product} />
+              <RelatedItems product={product} searchData={searchData} item={item}/>
+              <FedexTool product={product} />
+
             </div>
-            <button onClick={handlePin}>Save to Favs</button>
-            <SelfPromo product={product} />
-            <ColorBox product={product} />
-            <RelatedItems item={item} />
-            </>
           ))}
 
           
